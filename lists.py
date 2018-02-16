56ae72de330aff66a23ead2a057ffae39519af35
JMaling <jmaling@fwparker.org>
2018-02-16 09:00:34 -0600

    Lists Problem Set V1

--- lists.py
+++ lists.py
@@ -1,4 +1,5 @@
 #LISTS (31PTS TOTAL)
+import random
 #In these exercises you should should use functions as needed.  All functions should have comments to describe their purpose.
 
 # PROBLEM 1 (Using List Comprehensions - 6pts)
@@ -6,6 +7,12 @@
 # a) Make a list of numbers from 1 to 100
 # b) Make a list of even numbers from 20 to 40
 # c) Make a list of squares from 1 to 100 (1 ** 2 to 100 ** 2)
+num_list = [x for x in range(1, 101)]
+print(num_list)
+num_list2 = [x for x in num_list if x >= 20 and x <= 40 and x % 2 == 0]
+print(num_list2)
+sqr_list = [x ** 2 for x in num_list]
+print(sqr_list)
 
 #PROBLEM 2 (8-ball - 5pts)
 # A magic 8-ball, when asked a question, provides a random answer from a list.
@@ -18,13 +25,37 @@ yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
 now", "Cannot predict now", "Concentrate and ask again", "Don ' t \
 count on it", "My reply is no", "My sources say no", "Outlook \
 not so good", "Very doubtful" ]
-
+print("Welcome to the Magic 8-Ball!")
+input1 = input("Please Enter Your Question to Shake the Ball: ")
+print(answer_list[random.randrange(len(answer_list))])
+'''
+random1 = random.randrange(len(answer_list))
+for i in range(len(answer_list)):
+    if i == random1:
+        print(answer_list[i])
+'''
 
 # PROBLEM 3 (Shuffle - 5pts)
 # A playing card consists of a suit (Heart, Diamond, Club, Spade) and a value (2,3,4,5,6,7,8,9,10,J,Q,K,A).
 # Create a list of all possible playing cards, which is a deck.
 # Then create a function that shuffles the deck, producing a random order.
-
+deck = []
+suits = ["S", "C", "H", "D"]
+numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+for i in suits:
+    for num in numbers:
+        deck.append(num + i)
+#  print(deck)
+new_deck = []
+copy_deck = deck[:]
+done = False
+while not done:
+    draw = copy_deck.pop(random.randrange(len(copy_deck)))
+    new_deck.append(draw)
+    if len(copy_deck) == 0:
+        done = True
+print(new_deck)
+print(len(new_deck))
 
 # PROBLEM 4 (Tic-Tac-Toe - 15pts)
 # Write a Tic-Tac-Toe program that allows two people to play the game against each other.
@@ -60,6 +91,59 @@ not so good", "Very doubtful" ]
 #       announce draw
 #       break
 #   switch player
+done = False
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+player = "X"
+
+def draw_board(board):
+    print("Top Row =    ", board[0])
+    print("Middle Row = ", board[1])
+    print("Bottom Row = ", board[2])
+def check_win(board, player):
+    if board[0][0] == player and board[0][1] == player and board[0][2] == player:
+        return True
+    elif board[1][0] == player and board[1][1] == player and board[1][2] == player:
+        return True
+    elif board[2][0] == player and board[2][1] == player and board[2][2] == player:
+        return True
+    elif board[0][0] == player and board[1][0] == player and board[2][0] == player:
+        return True
+    elif board[0][1] == player and board[1][1] == player and board[2][1] == player:
+        return True
+    elif board[0][2] == player and board[1][2] == player and board[2][2] == player:
+        return True
+    elif board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    elif board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    else:
+        return False
+def check_full(board):
+    for row1 in board:
+        if " " in row1:
+            return False
+    else:
+        return True
+
+while not done:
+    row = int(input("Player " + player + " Enter Row (1, 2, 3): "))
+    col = int(input("Player " + player + " Enter Column (1, 2, 3): "))
+    if board[row - 1][col - 1] == " ":
+        board[row - 1][col - 1] = player
+    else:
+        print("That Space is Taken")
+        continue
+    draw_board(board)
+    if check_win(board, player):
+        print(player, "WINS")
+        break
+    if check_full(board):
+        print("DRAW")
+    if player == "X":
+        player = "O"
+    else:
+        player = "X"
+
 
 # CHALLENGE PROBLEM 5 (MAY DO AS SUBSTITUTE FOR PROBLEM 4, NO ADDITIONAL CREDIT)
 # Create a program that is a simplified version of the game “Battleship.”
